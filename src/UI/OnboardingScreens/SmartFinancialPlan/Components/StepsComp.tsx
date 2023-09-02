c975140e026ab6e87aa7e7f8f9a05cf4c4ee73f2
Tamoor Malik <tamoormalik088@gmail.com>
2023-09-02 12:35:48 +0500

    File Structure

---
+++ src/UI/OnboardingScreens/SmartFinancialPlan/Components/StepsComp.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StepsComp = () => {
+  return (
+    <View>
+      <Text>{step}</Text>
+    </View>
+  )
+}
+
+export default StepsComp
